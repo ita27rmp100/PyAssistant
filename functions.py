452c228479141536150f26b926cafec65d9f463d
N3dal <nedalxzo@gmail.com>
2023-03-29 12:02:08 +0200

    created Menu class

--- functions.py
+++ functions.py
@@ -1,20 +1,62 @@
-import webbrowser , os
+import webbrowser
+import os
 
-services = ['open an application','search about something','search in youtube','open a website']
+services = ['open an application', 'search about something',
+            'search in youtube', 'open a website']
+
 applications = {
-    'google chrome':'chrome.exe',
-    'my editor':"Visual Studio Code.lnk",
+    'google chrome': 'chrome.exe',
+    'my editor': "Visual Studio Code.lnk",
 }
 print('our services :')
-for j in services :
+for j in services:
     print(j)
 
-class Orders :
-    def OpenAnApp(app_name):
+
+class Menu:
+    """
+        Menu to view all the available services;
+
+        :ARGS:
+            None;
+
+    """
+
+    SERVICES = ['open an application', 'search about something',
+                'search in youtube', 'open a website']
+
+    def draw(self):
+        """
+            :ARGS:
+                None;
+
+            :RETURNS:
+                return None;
+
+            :INFO:
+                draw the menu on the terminal;
+        """
+
+        for index, service in enumerate(Menu.SERVICES, start=1):
+            print(f"[{index}] {service}")
+
+
+class Orders:
+    """
+        Consisting All the commands that can done by the script;
+
+        :ARGS:
+            None;
+
+    """
+    def OpenAnApp(app_name: str):
         os.startfile(applications[app_name])
-    def OpenWebSite(name):
+
+    def OpenWebSite(name: str):
         webbrowser.open(name)
-    def SearchAboutSomething(txt) :
+
+    def SearchAboutSomething(txt: str):
         webbrowser.open(f'https://www.google.com/search?q={txt}')
-    def SearchInYoutube(txt):
+
+    def SearchInYoutube(txt: str):
         webbrowser.open(f'https://www.youtube.com/results?search_query={txt}')
